22599500d882eb5130206810ed5a0eecb5bf2691
Nick Maslov <kategorychnost@gmail.com>
2023-03-13 18:27:17 -0400

    created Column, Card, AddNewItem form; styles

---
+++ notes.md
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+@types/styled-components nanoid react-dnd react-dnd-html5-backend styled-components throttle-debounce-ts use-immer immer
+
+@types/react-router-dom @types/yup yup nes.css node-sass react-hook-form react-router-dom
+
+clsx soundfont-player
+
+@reduxjs/toolkit @types/file-saver @types/react-redux @types/redux-logger file-saver react-redux redux-devtools-extension redux-logger xp.css 
---
+++ src/src_trello/Column.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ColumnContainer, ColumnTitle } from './styles';
+import { AddNewItem } from './AddNewItem';
+
+// type ColumnProps = {
+//     text: string;
+//     children?: React.ReactNode;
+// };
+type ColumnProps = React.PropsWithChildren<{
+    text: string;
+}>;
+
+export const Column = ({ text, children }: ColumnProps) => {
+    return (
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            {children}
+            <AddNewItem
+                toggleButtonText='+ Add another task'
+                onAdd={console.log}
+                dark
+            />
+        </ColumnContainer>
+    );
+};
