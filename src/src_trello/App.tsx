6493448d5b21cae086494b6110a7bcb05f196ccc
Nick Maslov <kategorychnost@gmail.com>
2023-03-13 21:41:07 -0400

    implementing global state managemnt

--- src/src_trello/App.tsx
+++ src/src_trello/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Column } from './Column';
-import { Card } from './Card';
 import { AppContainer } from './styles';
 import { AddNewItem } from './AddNewItem';
+import { AppStateProvider, useAppState } from './state/AppStateContext';
 
 export const App: React.FC = () => {
+    const { lists } = useAppState();
+
+    console.log(lists);
     return (
-        <AppContainer>
-            <Column text='To Do'>
-                <Card text='Generate app scaffold' />
-            </Column>
-            <Column text='In Progress'>
-                <Card text='Learn Typescript' />
-            </Column>
-            <Column text='Done'>
-                <Card text='Begin to use static typing' />
-            </Column>
-            <AddNewItem
-                toggleButtonText='+ Add another list'
-                onAdd={console.log}
-            />
-        </AppContainer>
+        <AppStateProvider>
+            <AppContainer>
+                {lists.map((list) => (
+                    <Column text={list.text} key={list.id} id={list.id} />
+                ))}
+                <AddNewItem
+                    toggleButtonText='+ Add another list'
+                    onAdd={console.log}
+                />
+            </AppContainer>
+        </AppStateProvider>
     );
 };
--- src/src_trello/Column.tsx
+++ src/src_trello/Column.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { ColumnContainer, ColumnTitle } from './styles';
 import { AddNewItem } from './AddNewItem';
 
+import { useAppState } from './state/AppStateContext';
+import { Card } from './Card';
+
 // type ColumnProps = {
 //     text: string;
 //     children?: React.ReactNode;
 // };
 type ColumnProps = React.PropsWithChildren<{
     text: string;
+    id: string;
 }>;
 
-export const Column = ({ text, children }: ColumnProps) => {
+export const Column = ({ text, children, id }: ColumnProps) => {
+    const { getTasksByListId } = useAppState();
+    const tasks = getTasksByListId(id);
+
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            {children}
+            {tasks.map((task) => (
+                <Card text={task.text} key={task.id} id={task.id} />
+            ))}
+
             <AddNewItem
                 toggleButtonText='+ Add another task'
                 onAdd={console.log}
